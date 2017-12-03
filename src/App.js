840e75bf09cc6201f9d06144a349f46c6ae2479a
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2017-12-03 10:21:25 -0300

    adding an event handler that handles the text that we add in the input field

--- src/App.js
+++ src/App.js
@@ -11,8 +11,16 @@ class App extends Component {
         {id: 1, name: 'Learn JSX', isComplete: true},
         {id: 2, name: 'Build an Awesome App', isComplete: flase},
         {id: 3, name: 'Ship It!', isComplete: flase},
-      ]
+      ],
+      currentTodo: '',
     }
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange (event){
+    this.setState({
+      currentTodo: event.target.value,
+    })
   }
 
   render() {
@@ -24,7 +32,7 @@ class App extends Component {
         </header>
         <div className="Todo-App">
           <form>
-            <input type="text"/>
+            <input type="text" onChange={this.handleInputChange} value={this.state.currentTodo}/>
           </form>
           <div className="Todo-List">
             <ul>
